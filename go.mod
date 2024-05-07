3ccf017993cb1463d2a5ffce6596c7e13a477a37
zelinskayas <sveta455@mail.ru>
2024-05-07 03:15:19 +0300

    Version v2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zelinskayas/Lets_GO_ibolit
+module github.com/zelinskayas/Lets_GO_ibolit/v2
 
 go 1.21.1
--- ibolit.go
+++ ibolit.go
@@ -2,27 +2,18 @@ package Lets_GO_ibolit
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"os"
-	"sort"
 )
 
-type animal struct {
+type Patient struct {
 	Name  string `json:"name"`
 	Age   int    `json:"age"`
 	Email string `json:"email"`
 }
 
-// sort
-type ByAge []animal
-
-func (a ByAge) Len() int {
-	return len(a)
-}
-func (a ByAge) Swap(i, j int) {
-	a[i], a[j] = a[j], a[i]
-}
-func (a ByAge) Less(i, j int) bool {
-	return a[i].Age < a[j].Age
+type patients struct {
+	List []Patient `xml:"Patient"`
 }
 
 func Do(sourcefile, resultfile string) error {
@@ -32,18 +23,15 @@ func Do(sourcefile, resultfile string) error {
 	}
 	defer f.Close()
 	dec := json.NewDecoder(f)
-	res := make([]animal, 0, 3)
+	p := patients{}
 	for dec.More() {
-		var a animal
+		var a Patient
 		err = dec.Decode(&a)
 		if err != nil {
 			return err
 		}
-		res = append(res, a)
+		p.List = append(p.List, a)
 	}
-
-	//sort by age
-	sort.Sort(ByAge(res))
 
 	fnew, err := os.Create(resultfile)
 	if err != nil {
@@ -51,30 +39,14 @@ func Do(sourcefile, resultfile string) error {
 	}
 	defer fnew.Close()
 
-	_, err = fnew.WriteString("[")
+	_, err = fnew.WriteString(xml.Header)
 	if err != nil {
 		return err
 	}
 
-	for i, anim := range res {
-		jsonData, err := json.Marshal(anim)
-		if err != nil {
-			return err
-		}
-		_, err = fnew.Write(jsonData)
-		if err != nil {
-			return err
-		}
-
-		if i < len(res)-1 {
-			_, err = fnew.WriteString(",\n")
-		}
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fnew.WriteString("]")
+	enc := xml.NewEncoder(fnew)
+	enc.Indent("", " ")
+	err = enc.Encode(p)
 	if err != nil {
 		return err
 	}
